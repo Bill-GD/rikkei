7d00779760b6eaa485e579394bec888d99b78e23
Bill-GD <geometrydashbill@gmail.com>
2024-10-28 14:42:43 +0700

    find max of 3

--- session_1/max_number.js
+++ session_1/max_number.js
@@ -1,3 +1,9 @@
 function findMax() {
+  const a = parseInt(prompt('Enter a:')),
+    b = parseInt(prompt('Enter b:')),
+    c = parseInt(prompt('Enter c:'));
 
+  const max = a > b ? (a > c ? a : c) : (b > c ? b : c);
+  console.log(`Số lớn nhất là - ${max}`);
+  alert(`Số lớn nhất là - ${max}`);
 }
