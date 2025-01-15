9df9ff30a1e47875fa5c57fac060b45481c6ac15
Bill-GD <geometrydashbill@gmail.com>
2025-01-15 10:36:04 +0700

    node s1 finished

--- node/exercise_1/server.js
+++ node/exercise_1/server.js
@@ -11,29 +11,96 @@ const server = createServer((req, res) => {
   //   'utf8',
   // ));
 
-  // res.setHeader('Content-Type', 'text/html; charset=utf8');
-  res.setHeader('Content-Type', 'application/json; charset=utf8');
+  res.setHeader('Content-Type', 'text/html; charset=utf8');
+  // res.setHeader('Content-Type', 'application/json; charset=utf8');
+  const reqUrlParts = req.url.split('/').filter(value => value.length > 0).map(String);
 
   let response;
-  switch (req.url) {
+  switch (`/${reqUrlParts[0] ?? ''}`) {
     case '/':
-      response = '<h1>This is homepage</h1>';
+    case '/overview': {
+      const data = JSON.parse(fs.readFileSync(
+        './dev-data/data.json',
+        'utf8',
+      ));
+
+      let overviewContent = fs.readFileSync(
+        './templates/overview.html',
+        'utf8',
+      );
+
+      const cards = [];
+      if (data instanceof Array && data.length > 0) {
+        for (const item of data) {
+          let cardTemplate = fs.readFileSync(
+            './templates/card-template.html',
+            'utf8',
+          );
+
+          Object.keys(item).forEach(key => {
+            cardTemplate = cardTemplate.replaceAll(`{{${key}}}`, item[key]);
+          });
+          cards.push(cardTemplate);
+        }
+        overviewContent = overviewContent.replaceAll('{{cards}}', cards.join(''));
+      }
+
+      response = overviewContent;
       break;
-    case '/overview':
-      response = '<h1>This is overview page</h1>';
+    }
+    case '/product': {
+      if (!reqUrlParts[1]) {
+        response = '<h1>PAGE NOT FOUND</h1>';
+        break;
+      }
+
+      let prodTemplate = fs.readFileSync(
+        './templates/product.html',
+        'utf8',
+      );
+
+      const data = JSON.parse(fs.readFileSync(
+        './dev-data/data.json',
+        'utf8',
+      )).find(item => `${item['id']}` === reqUrlParts[1]);
+
+      if (data && data instanceof Object) {
+        data['organic'] = data['organic'] ? 'organic' : 'inorganic';
+        Object.keys(data).forEach(key => {
+          prodTemplate = prodTemplate.replaceAll(`{{${key}}}`, data[key]);
+        });
+      } else {
+        response = '<h1>PRODUCT NOT FOUND</h1>';
+        break;
+      }
+      response = prodTemplate;
       break;
-    case '/product':
-      response = '<h1>This is product page</h1>';
-      break;
-    case '/api':
-      response = fs.readFileSync(
+    }
+    case '/api': {
+      res.setHeader('Content-Type', 'application/json; charset=utf8');
+      const data = fs.readFileSync(
         './dev-data/data.json',
         'utf8',
       );
+      if (!reqUrlParts[1]) {
+        response = data;
+        break;
+      }
+
+      const json = JSON.parse(data);
+      const id = parseInt(reqUrlParts[1]);
+      if (isNaN(id) || id < 0 || id >= json.length) {
+        response = '<h1>PAGE NOT FOUND</h1>';
+        break;
+      }
+
+      response = JSON.stringify(json[id]);
       break;
-    default:
+    }
+    default: {
       response = '<h1>PAGE NOT FOUND</h1>';
       break;
+    }
   }
   res.end(response);
 });
