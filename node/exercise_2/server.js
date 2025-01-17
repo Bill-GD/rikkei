470741ce1db1c8ffb8132b2aff7c86c498d82c5a
Bill-GD <geometrydashbill@gmail.com>
2025-01-18 00:17:28 +0700

    node ex2 search

--- node/exercise_2/server.js
+++ node/exercise_2/server.js
@@ -1,5 +1,6 @@
 const { readFileSync } = require('node:fs');
 const { createServer } = require('node:http');
+const URL = require('node:url');
 
 const host = '127.0.0.1', port = 3000;
 
@@ -8,7 +9,9 @@ const server = createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf8');
 
   let response;
-  const urlParts = req.url.split('/').filter(e => e.length > 0).map(String);
+  const parsedUrl = URL.parse(req.url);
+  const urlParts = parsedUrl.pathname.split('/').filter(e => e.length > 0).map(String);
+  // console.log(parsedUrl);
 
   switch (`/${urlParts[0] ?? ''}`) {
     case '/':
@@ -35,10 +38,45 @@ const server = createServer((req, res) => {
       }
       const item = JSON.parse(readFileSync('./dev-data/data.json', 'utf8')).find(e => `${e.id}` === urlParts[1]);
       item['organic'] = item['organic'] ? 'organic' : 'inorganic';
+      if (!item) {
+        res.statusCode = 404;
+        response = '<h1>PRODUCT NOT FOUND</h1>';
+        break;
+      }
 
       let product = readFileSync('./templates/product.html', 'utf8');
       Object.keys(item).forEach(key => product = product.replaceAll(`{{${key}}}`, item[key]));
       response = product;
+      break;
+    }
+    case '/search': {
+      // const queries = {};
+      // parsedUrl.query.split('&').map(e => {
+      //   const [k, v] = e.split('=');
+      //   queries[`${k}`] ??= v;
+      // });
+      const queries = new URLSearchParams(parsedUrl.query);
+      let search = readFileSync('./templates/search.html', 'utf8');
+
+      if (queries.size > 0) {
+        console.log(queries);
+        const item = JSON.parse(readFileSync('./dev-data/data.json', 'utf8')).find(e => e.productName === queries.get('p').trim());
+        if (!item) {
+          search = search.replaceAll('{{message}}', 'NOT FOUND');
+          response = search;
+          break;
+        }
+
+        // item['organic'] = item['organic'] ? 'organic' : 'inorganic';
+        // let product = readFileSync('./templates/product.html', 'utf8');
+        // Object.keys(item).forEach(key => product = product.replaceAll(`{{${key}}}`, item[key]));
+
+        res.writeHead(302, { 'Location': `/product/${item.id}` });
+        break;
+      }
+
+      search = search.replaceAll('{{message}}', 'Find your fruits');
+      response = search;
       break;
     }
     case '/api': {
